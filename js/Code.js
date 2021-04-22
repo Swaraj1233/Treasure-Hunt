88b4b8eea243ba3bc57557cbc11fedc8f7243b13
Swaraj1233 <75794477+Swaraj1233@users.noreply.github.com>
2021-04-22 19:48:33 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "WHITEHAT";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("T I O F N C N U", 700,150)
+    fill("lightblue")
+    text("Hint: Performs the task", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("T H A H I E T W", 100,250)
+    fill("lightblue")
+    text("Hint: A coding platform!!", 100,270)
+}
